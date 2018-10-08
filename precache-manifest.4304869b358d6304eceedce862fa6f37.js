d58401065d558f347ab5fbfdfba87a8e029f3478
Shubalub Kingz <atstay@gmail.com>
2018-10-07 20:30:31 -0600

    Updates

---
+++ precache-manifest.4304869b358d6304eceedce862fa6f37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfffd8a183a2bdd799c7",
+    "url": "/ClickyReact/static/js/runtime~main.dfffd8a1.js"
+  },
+  {
+    "revision": "ff7d84732d0e1e6a2f99",
+    "url": "/ClickyReact/static/js/main.ff7d8473.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/ClickyReact/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "ff7d84732d0e1e6a2f99",
+    "url": "/ClickyReact/static/css/main.d2d09453.chunk.css"
+  },
+  {
+    "revision": "18987ae8d575b06b806ef72f341b76bc",
+    "url": "/ClickyReact/index.html"
+  }
+];
